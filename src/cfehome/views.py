8af1d0518b926c2fca87c58383f32b814b94a8be
vidusanka98 <vidusanka98@gmail.com>
2024-10-16 20:27:58 +0530

    hello world

---
+++ src/cfehome/views.py
@@ -0,0 +1,52 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+from visits.models import PageVisit
+this_dir=pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request,*args,**kwargs):
+    qs=PageVisit.objects.all()
+    page_qs=PageVisit.objects.filter(path=request.path)
+
+    my_title="My Page"
+
+    my_context={
+        "page_title":my_title,
+        "page_visits_count":page_qs.count(),
+        "percent":(page_qs.count()*100.0)/qs.count(),
+        "total_visits":qs.count()
+    }
+    
+    path=request.path
+  
+    html_template="home.html"
+    PageVisit.objects.create(path=request.path)
+    #html_file_path=this_dir/"home.html"
+   # html_=html_file_path.read_text()
+
+    return render(request,html_template,my_context)
+
+def home_page_view_old (request,*args,**kwargs):
+    my_title="My Page"
+
+    my_context={
+        "page_title":my_title
+    }
+  
+    html_="""
+<!DOCTYPE html>
+<html>
+
+<body>
+<h1>{page_title}Inline is anything</h1>
+</body>
+
+    </html>
+""".format(**my_context)
+    #html_file_path=this_dir/"home.html"
+   # html_=html_file_path.read_text()
+
+    return HttpResponse(html_)
+    
+  
+  
---
+++ src/visits/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class PageVisit(models.Model):
+    #db->table
+    #id ->primary hidden key ->autofield ->1,2,3,4,5
+    path=models.TextField(blank=True,null=True) #col
+    timestamp=models.DateTimeField(auto_now_add=True) #col
+    
