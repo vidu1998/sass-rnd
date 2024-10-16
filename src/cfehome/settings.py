203f2ddfa55ee0da4e2f9b15c4c406ca8eb26a6f
vidusanka98 <vidusanka98@gmail.com>
2024-10-16 20:59:12 +0530

    Prepare for Railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,8 +25,14 @@ SECRET_KEY = 'django-insecure-x62sm+(w5hhxj7_8#*s(+i&=9e@ryz(!77un#tqdckve9w8ho)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 # Application definition
 
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -4,10 +4,19 @@ from django.http import HttpResponse
 from visits.models import PageVisit
 this_dir=pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request,*args,**kwargs):
+def home_view(request,*args,**kwargs):
+    return about_view(request,*args,**kwargs)
+
+def about_view(request,*args,**kwargs):
     qs=PageVisit.objects.all()
     page_qs=PageVisit.objects.filter(path=request.path)
+    try:
+              
+        
+        percent=(page_qs.count()*100.0)/qs.count(),
 
+    except:
+        percent=0    
     my_title="My Page"
     html_template="home.html"
 
@@ -26,7 +35,6 @@ def home_page_view(request,*args,**kwargs):
    # html_=html_file_path.read_text()
 
     return render(request,html_template,my_context)
-
 def home_page_view_old (request,*args,**kwargs):
     my_title="My Page"
 
