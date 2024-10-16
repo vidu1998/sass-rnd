f0bb7482e6b34d25920138b1b9bfac942319d4a5
vidusanka98 <vidusanka98@gmail.com>
2024-10-16 20:36:31 +0530

    add html templete

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -9,6 +9,7 @@ def home_page_view(request,*args,**kwargs):
     page_qs=PageVisit.objects.filter(path=request.path)
 
     my_title="My Page"
+    html_template="home.html"
 
     my_context={
         "page_title":my_title,
